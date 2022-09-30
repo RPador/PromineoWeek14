8c3101c2000be0dff99ca26df171416cbb001b4f
Ranon Pador <108454239+RPador@users.noreply.github.com>
2022-09-29 20:26:03 -0700

    Add files via upload

---
+++ java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author Rainy P
+ *
+ */
+public enum JeepModel {
+  WRANGLER, GRAND_CHEROKEE, CHEROKEE, COMPASS, RENEGADE, GLADIATOR
+}
